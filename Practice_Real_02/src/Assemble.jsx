ce3c43ca6ab756bf4cf24400256bb7728fc6b9a0
CodeByMukul-Tech <mukulvats200@gmail.com>
2025-01-16 17:30:33 +0530

    assemble is used to assemeble the components using the componet section

---
+++ Practice_Real_02/src/Assemble.jsx
@@ -0,0 +1,12 @@
+import Component_1 from "./Component/Component_1";
+import { Fragment } from "react";
+function Assemble(){
+    return(
+        <Fragment>
+            <Component_1/>
+        </Fragment>
+       
+    )
+}
+
+export default Assemble ;
---
+++ Practice_Real_02/src/Component/Component_1.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+function Component_1(){
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default Component_1 ; 
